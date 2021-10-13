891ae98d81991965650bd49c7e758c30bad26700
Pavel <sa1zen@mail.ru>
2021-10-13 19:13:24 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int x = 5, y = 3;
+    int z = ++x;
+    y += x--;
+    z+= --x +y++;
+    System.out.println(x+y+z);
+    }
+}
